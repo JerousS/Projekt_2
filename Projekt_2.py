84e39a5d2bf119bdc8d75ac7705f1918ad4cec07
JerousS <JerousS@users.noreply.github.com>
2023-02-01 15:04:05 +0100

    fourth commit

--- Projekt_2.py
+++ Projekt_2.py
@@ -1,9 +1,3 @@
-"""
-projekt_2.py: druhý projekt do Engeto Online Python Akademie
-author: Jaroslav Shanel
-email: jarekshanel@gmail.com
-discord: JaroslavS
-"""
 import random
 oddelovac = "-----------------------------------------------"
 print("Hi there!", oddelovac, sep="\n")
@@ -18,13 +12,32 @@ while len(number) !=4:
     if number[0] == 0:
         number.remove(0)
 random_number = "".join([str(i) for i in number])
-
+hr = []
 print(random_number)
 guess = ""
+num_of_guesses = 0
 while guess != random_number:
     guess = input()
-    
-
-    
-
-    
+    start_time = time.time()
+    num_of_guesses+=1
+    if len(guess) != 4 or str(guess[0]) == "0" or not guess.isnumeric():
+        print("Please put 4digit number not starting with 0")
+        continue
+    for i in str(guess):
+        if not i in hr:
+            hr.append(i)
+        else:
+            print("4digit number with no duplicites please")
+            hr.clear()
+            break
+    else:
+        bull = 0
+        cow = 0
+        for i in range(4):
+            if guess[i] == random_number[i]:
+                bull+=1
+            elif guess[i] in random_number:
+                cow+=1
+        print(bull, "bulls" if bull > 1 else "bull",",", cow, "cows" if cow>1 else "cow")
+else:
+    print("Correct, you've guessed the right number in",num_of_guesses, "guesses!")    
