de3b4d3dd705e6d3d7dca0f81f6417c38a086c34
JerousS <JerousS@users.noreply.github.com>
2023-02-01 11:43:45 +0100

    second commit

--- Projekt_2.py
+++ Projekt_2.py
@@ -4,3 +4,22 @@ author: Jaroslav Shanel
 email: jarekshanel@gmail.com
 discord: JaroslavS
 """
+import random
+oddelovac = "-----------------------------------------------"
+print("Hi there", oddelovac, sep="\n")
+
+number = []
+while len(number) !=4:
+    i=random.randint(0,9)
+    if i not in number:   
+        number.append(i)
+    if number[0] == 0:
+        number.remove(0)
+kra = "".join(str(number))
+random_number = "".join([str(i) for i in number])
+print(random_number)
+print("""I've generated a random 4 digit number for you.
+Let's play a bulls and cows game.""", oddelovac, sep="\n")
+print("Enter a number:", oddelovac, sep="\n")
+guess = ""
+# while 
